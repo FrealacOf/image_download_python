48116b85b8e58c6bdbf2263465b6236a5ff4a14c
FrealacOf <85633397+FrealacOf@users.noreply.github.com>
2021-10-07 07:10:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,44 @@
-import os
-import keyboard
 from selenium import webdriver
-import time
-import os
+import time, requests
 
-token_ask = input("C'est quoi le token ? > ")
+ask_type = input("Vous voulez telecharger quoi ? \n > ")
 
+def search_google(search_query):
+    browser = webdriver.Chrome()
+    search_url = f"https://www.google.com/search?site=&tbm=isch&source=hp&biw=1873&bih=990&q={search_query}"
+    images_url = []
 
-def login():
-    keyboard.write("function login(token) {")
-    keyboard.write("setInterval(() => {")
-    keyboard.write(
-        'document.body.appendChild(document.createElement `iframe`).contentWindow.localStorage.token = `"${token}"`')
-    keyboard.write("}, 50);")
-    keyboard.write("setTimeout(() => {")
-    keyboard.write("location.reload();")
-    keyboard.write("}, 200);")
-    keyboard.write("}")
-    keyboard.write('login("{}")'.format(token_ask))
-    time.sleep(0.5)
-    keyboard.press_and_release("enter")
+    # open browser and begin search
+    browser.get(search_url)
+    elements = browser.find_elements_by_class_name('rg_i')
 
+    count = 0
+    for e in elements:
+        # get images source url
+        e.click()
+        time.sleep(1)
+        element = browser.find_elements_by_class_name('v4dQwb')
 
-driver = webdriver.Chrome(executable_path="chromedriver.exe")
-driver.get("https://discord.com/login")
-time.sleep(2)
-keyboard.press_and_release("ctrl+shift+j")
-time.sleep(2)
-login()
+        # Google image web site logic
+        if count == 0:
+            big_img = element[0].find_element_by_class_name('n3VNCb')
+        else:
+           big_img = element[1].find_element_by_class_name('n3VNCb')
+
+        images_url.append(big_img.get_attribute("src"))
+
+        # write image to file
+        reponse = requests.get(images_url[count])
+        if reponse.status_code == 200:
+            with open(f"search{count+1}.jpg","wb") as file:
+                file.write(reponse.content)
+
+        count += 1
+
+        # Stop get and save after 5
+        if count == 100: # you can change this / Vous pouvez changer ceci
+            break
+
+    return images_url
+
+items = search_google(ask_type)
